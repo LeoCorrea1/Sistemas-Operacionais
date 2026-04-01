7af870eb01fc9137d4d68e449883c03073f29257
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-04-01 08:16:40 -0300

    Refactor Exercicio1 class and update thread logic

--- Aula5-Lista2/Exercicio1.java
+++ Aula5-Lista2/Exercicio1.java
@@ -1,4 +1,21 @@
-class ContadorPausado extends Thread {
+public class Exercicio1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Número: " + i);
+            try {
+                Thread.sleep(1000); // Pausa de 1 segundo
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercicio1 t1 = new Exercicio1();
+        t1.start();
+    }
+}class ContadorPausado extends Thread {
     public void run() {
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
