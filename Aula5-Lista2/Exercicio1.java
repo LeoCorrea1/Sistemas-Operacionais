ce6a844435383a636ec9899c03f22a862c74f0dc
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-04-01 08:11:48 -0300

    Fix formatting issues in Exercicio1.java

---
+++ Aula5-Lista2/Exercicio1.java
@@ -0,0 +1,12 @@
+class ContadorPausado extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrompida");
+            }
+        }
+    }
+}
