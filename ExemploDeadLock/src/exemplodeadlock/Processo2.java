ee01ba95d0025895bdceacd35275db736e5ffc81
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-05-20 09:17:59 -0300

    Add files via upload

---
+++ ExemploDeadLock/src/exemplodeadlock/Processo2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplodeadlock;
+
+
+public class Processo2 extends Thread {
+
+    private Recurso recurso1;
+    private Recurso recurso2;
+
+    public Processo2(Recurso r1, Recurso r2) {
+        this.recurso1 = r1;
+        this.recurso2 = r2;
+    }
+
+    @Override
+    public void run() {
+        synchronized (recurso1) {
+            System.out.println("Processo 2 bloqueou " + recurso2.getNome());
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+            
+            synchronized (recurso2) {
+                System.out.println("Processo 2 bloqueou " + recurso1.getNome());
+            }
+        }
+    }
+}
---
+++ ExemploDeadLock/src/exemplodeadlock/Recurso.java
@@ -0,0 +1,15 @@
+
+package exemplodeadlock;
+
+public class Recurso {
+    
+    private String nome;
+    
+    public Recurso(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+}
